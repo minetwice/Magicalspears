ca56fce9b2e6dd5176f6634d858e2f4af406edac
minetwice <ytd82774@gmail.com>
2025-10-23 22:25:07 +0530

    Update GiveSpearCommand.java

--- src/main/java/com/minetwice/magicalspears/commands/GiveSpearCommand.java
+++ src/main/java/com/minetwice/magicalspears/commands/GiveSpearCommand.java
@@ -1,1 +1,43 @@
 
+package com.minetwice.magicalspears.commands;
+
+import com.minetwice.magicalspears.MagicalSpears;
+import com.minetwice.magicalspears.managers.SpearManager;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class GiveSpearCommand implements CommandExecutor {
+
+    private final MagicalSpears plugin;
+
+    public GiveSpearCommand(MagicalSpears plugin) {
+        this.plugin = plugin;
+    }
+
+    // usage: /givespear <player> <spearname>
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 2) {
+            sender.sendMessage(Component.text("Usage: /givespear <player> <INFERNO|FROST|STORM|SOUL|WIND|TIDAL>"));
+            return true;
+        }
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage(Component.text("Player not found."));
+            return true;
+        }
+        try {
+            SpearManager.SpearType type = SpearManager.SpearType.valueOf(args[1].toUpperCase());
+            ItemStack spear = plugin.getSpearManager().getSpear(type);
+            target.getInventory().addItem(spear);
+            sender.sendMessage(Component.text("Gave " + type.name() + " to " + target.getName()));
+            target.sendMessage(Component.text("You received " + type.name()));
+        } catch (IllegalArgumentException ex) {
+            sender.sendMessage(Component.text("Invalid spear name."));
+        }
+        return true;
+    }
+}
