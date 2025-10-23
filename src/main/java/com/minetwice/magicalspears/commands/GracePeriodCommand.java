6fc2e44d73d058f8a586da0806a4a51e4ae9e4d5
minetwice <ytd82774@gmail.com>
2025-10-23 22:27:12 +0530

    Update GracePeriodCommand.java

--- src/main/java/com/minetwice/magicalspears/commands/GracePeriodCommand.java
+++ src/main/java/com/minetwice/magicalspears/commands/GracePeriodCommand.java
@@ -1,1 +1,32 @@
+package com.minetwice.magicalspears.commands;
+
+import com.minetwice.magicalspears.MagicalSpears;
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.*;
+
+public class GracePeriodCommand implements CommandExecutor {
+
+    private final MagicalSpears plugin;
+
+    public GracePeriodCommand(MagicalSpears plugin) {
+        this.plugin = plugin;
+    }
+
+    // /grace -> toggles start 15 minutes
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (plugin.isGraceActive()) {
+            sender.sendMessage(Component.text("Grace is already active."));
+            return true;
+        }
+        plugin.setGraceActive(true);
+        plugin.getServer().broadcast(Component.text("Grace period started: 15 minutes. PvP is disabled."));
+        // schedule end
+        plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
+            plugin.setGraceActive(false);
+            plugin.getServer().broadcast(Component.text("Grace period ended. PvP is enabled."));
+        }, 15 * 60 * 20L);
+        return true;
+    }
+}
 
