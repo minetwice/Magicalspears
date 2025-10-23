33342b06c0c3b16f8d332a9af6caee2fcb5935e9
minetwice <ytd82774@gmail.com>
2025-10-23 22:26:43 +0530

    Update SpearsGUICommand.java

--- src/main/java/com/minetwice/magicalspears/commands/SpearsGUICommand.java
+++ src/main/java/com/minetwice/magicalspears/commands/SpearsGUICommand.java
@@ -1,1 +1,34 @@
+package com.minetwice.magicalspears.commands;
+
+import com.minetwice.magicalspears.MagicalSpears;
+import com.minetwice.magicalspears.managers.SpearManager;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class SpearsGUICommand implements CommandExecutor {
+
+    private final MagicalSpears plugin;
+
+    public SpearsGUICommand(MagicalSpears plugin) {
+        this.plugin = plugin;
+    }
+
+    // /spears
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Component.text("Only players can use this."));
+            return true;
+        }
+        Player p = (Player) sender;
+        Inventory inv = Bukkit.createInventory(null, 9, Component.text("Magical Spears").toString());
+        for (ItemStack s : plugin.getSpearManager().getAllSpears()) inv.addItem(s);
+        p.openInventory(inv);
+        return true;
+    }
+}
 
