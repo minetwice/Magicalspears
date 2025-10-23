8e6c420ac228a637002c0608ae7cd3f53bc2def5
minetwice <ytd82774@gmail.com>
2025-10-23 22:25:35 +0530

    Update GiveAllSpearsCommand.java

--- src/main/java/com/minetwice/magicalspears/commands/GiveAllSpearsCommand.java
+++ src/main/java/com/minetwice/magicalspears/commands/GiveAllSpearsCommand.java
@@ -1,1 +1,38 @@
+package com.minetwice.magicalspears.commands;
+
+import com.minetwice.magicalspears.MagicalSpears;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class GiveAllSpearsCommand implements CommandExecutor {
+
+    private final MagicalSpears plugin;
+
+    public GiveAllSpearsCommand(MagicalSpears plugin) {
+        this.plugin = plugin;
+    }
+
+    // usage: /givespears <player>
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(Component.text("Usage: /givespears <player>"));
+            return true;
+        }
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage(Component.text("Player not found."));
+            return true;
+        }
+        for (ItemStack s : plugin.getSpearManager().getAllSpears()) {
+            target.getInventory().addItem(s);
+        }
+        sender.sendMessage(Component.text("Gave all spears to " + target.getName()));
+        target.sendMessage(Component.text("You received all magical spears."));
+        return true;
+    }
+}
 
