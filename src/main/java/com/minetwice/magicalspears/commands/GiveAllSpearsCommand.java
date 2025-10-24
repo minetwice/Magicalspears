386afdf4293898f485670a38cc6b7d7702862ca4
minetwice <ytd82774@gmail.com>
2025-10-24 07:17:40 +0530

    Update GiveAllSpearsCommand.java

--- src/main/java/com/minetwice/magicalspears/commands/GiveAllSpearsCommand.java
+++ src/main/java/com/minetwice/magicalspears/commands/GiveAllSpearsCommand.java
@@ -1,38 +1,55 @@
 package com.minetwice.magicalspears.commands;
 
 import com.minetwice.magicalspears.MagicalSpears;
-import net.kyori.adventure.text.Component;
+import com.minetwice.magicalspears.managers.SpearManager;
 import org.bukkit.Bukkit;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class GiveAllSpearsCommand implements CommandExecutor {
-
+    
     private final MagicalSpears plugin;
-
-    public GiveAllSpearsCommand(MagicalSpears plugin) {
+    private final SpearManager spearManager;
+    
+    public GiveAllSpearsCommand(MagicalSpears plugin, SpearManager spearManager) {
         this.plugin = plugin;
+        this.spearManager = spearManager;
     }
-
-    // usage: /givespears <player>
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length < 1) {
-            sender.sendMessage(Component.text("Usage: /givespears <player>"));
+        if (!sender.hasPermission("magicalspears.giveall")) {
+            sender.sendMessage("§cYou don't have permission to use this command!");
             return true;
         }
-        Player target = Bukkit.getPlayer(args[0]);
-        if (target == null) {
-            sender.sendMessage(Component.text("Player not found."));
-            return true;
+        
+        Player target;
+        if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("§cYou must specify a player from console!");
+                return true;
+            }
+            target = (Player) sender;
+        } else {
+            target = Bukkit.getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage("§cPlayer not found!");
+                return true;
+            }
         }
-        for (ItemStack s : plugin.getSpearManager().getAllSpears()) {
-            target.getInventory().addItem(s);
+        
+        for (ItemStack spear : spearManager.getAllSpears()) {
+            target.getInventory().addItem(spear);
         }
-        sender.sendMessage(Component.text("Gave all spears to " + target.getName()));
-        target.sendMessage(Component.text("You received all magical spears."));
+        
+        target.sendMessage("§aYou received all magical spears!");
+        if (sender != target) {
+            sender.sendMessage("§aGave " + target.getName() + " all magical spears!");
+        }
+        
         return true;
     }
 }
-
